302806c91d02793a5e663924b13b52f64b8f6e22
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-18 11:46:58 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 97.61%] - LeetPush

--- 198-House-Robber.cpp
+++ 198-House-Robber.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<int>dp;
     int solve(vector<int>& nums,int i,int n){
@@ -13,5 +13,28 @@ public:
         int n=nums.size();
         dp.assign(n,-1);
         return solve(nums,0,n);
+    }
+};
+*/
+class Solution {
+public:
+   
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)return 0;
+        if(n==1){
+            return nums[n-1];
+        }
+        int prev2=nums[0];
+        int prev1=max(nums[1],nums[0]);
+        
+        for(int i=2;i<n;i++){
+            int current=max(nums[i]+prev2,prev1);
+            
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+
     }
 };
