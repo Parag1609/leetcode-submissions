2c19539dd5c8b29409b774a68bb34378c9dd93bb
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-21 17:14:24 +0530

    [300] [Time Beats: 68.54%] [Memory Beats: 61.82%] - LeetPush

--- 300-Longest-Increasing-Subsequence.cpp
+++ 300-Longest-Increasing-Subsequence.cpp
@@ -1,3 +1,5 @@
+/*
+//recursion+memoization
 class Solution {
 public:
     vector<vector<int>>dp;
@@ -16,5 +18,25 @@ public:
         int n=nums.size();
         dp.assign(n,vector<int>(n+1,-1));
         return solve(nums,0,-1);
+    }
+};
+*/
+//bottom-up
+class Solution {
+public:
+   
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,1);
+        int maxlis=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+            maxlis=max(maxlis,dp[i]);
+        }
+       return maxlis;
     }
 };
