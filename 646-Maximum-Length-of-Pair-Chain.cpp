e6342d01a391b0818b07d451a5c9571539c2f7ba
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-21 18:04:48 +0530

    [646] [Time Beats: 42.88%] [Memory Beats: 38.96%] - LeetPush

--- 646-Maximum-Length-of-Pair-Chain.cpp
+++ 646-Maximum-Length-of-Pair-Chain.cpp
@@ -7,7 +7,6 @@ public:
         int lchain=1;
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(j==i)continue;
                 if(pairs[j][1]<pairs[i][0]){
                     dp[i]=max(dp[i],1+dp[j]);
                 }
