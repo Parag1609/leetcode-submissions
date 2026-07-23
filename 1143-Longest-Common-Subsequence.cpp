a1ca448db45de1ce3e078b012d7bd1d30fff8a4b
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-23 17:29:20 +0530

    [1143] [Time Beats: 28.01%] [Memory Beats: 31.53%] - LeetPush

--- 1143-Longest-Common-Subsequence.cpp
+++ 1143-Longest-Common-Subsequence.cpp
@@ -1,18 +1,17 @@
+
 class Solution {
 public:
     vector<vector<int>>dp;
     int solve(int idx1,int idx2,string& text1,string& text2){
         if(idx1>=text1.size()||idx2>=text2.size())return 0;
         if(dp[idx1][idx2]!=-1)return dp[idx1][idx2];
-        int takeboth=0;
-        int take1=0;
-        int take2=0;
+
         if(text1[idx1]==text2[idx2]){
-            takeboth=1+solve(idx1+1,idx2+1,text1,text2);
+            return 1+solve(idx1+1,idx2+1,text1,text2);
         }
-        take1=solve(idx1,idx2+1,text1,text2);
-        take2=solve(idx1+1,idx2,text1,text2);
-        return dp[idx1][idx2] = max(takeboth,max(take1,take2));
+        int take1=solve(idx1,idx2+1,text1,text2);
+        int take2=solve(idx1+1,idx2,text1,text2);
+        return dp[idx1][idx2] = max(take1,take2);
 
     }
     int longestCommonSubsequence(string text1, string text2) {
