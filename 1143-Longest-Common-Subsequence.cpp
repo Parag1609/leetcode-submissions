4da5c6aadc946524561f7418b652d7a5ddacaea1
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-23 19:37:38 +0530

    [1143] [Time Beats: 81.51%] [Memory Beats: 79.43%] - LeetPush

--- 1143-Longest-Common-Subsequence.cpp
+++ 1143-Longest-Common-Subsequence.cpp
@@ -22,23 +22,27 @@ public:
     }
 };
 */
-//bottom-up
+//space optimized
 class Solution {
 public:
-    vector<vector<int>>dp;
+    
     int longestCommonSubsequence(string text1, string text2) {
-        if(text1.size()==0||text2.size()==0)return 0;
-        dp.assign(text1.size()+1,vector<int>(text2.size()+1,0));
+        int m=text1.size();
+        int n=text2.size();
+        if(m==0||n==0)return 0;
+        vector<int>prev(n+1,0);
 
-        for(int i=1;i<=text1.size();i++){
-            for(int j=1;j<=text2.size();j++){
+        for(int i=1;i<=m;i++){
+            vector<int>curr(n+1,0);
+            for(int j=1;j<=n;j++){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+1;
+                   curr[j]=prev[j-1]+1;
                 }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=max(prev[j],curr[j-1]);
                 }
             }
+            prev=curr;
         }
-        return dp[text1.size()][text2.size()];
+        return prev[n];
     }
 };
