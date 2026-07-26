fa35337216ecef2c1e59eb73cbae45d7bc900468
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-26 23:22:24 +0530

    [5] [Time Beats: 16.67%] [Memory Beats: 8.28%] - LeetPush

--- 5-Longest-Palindromic-Substring.cpp
+++ 5-Longest-Palindromic-Substring.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool solve(string& s,int i,int j){
-        if(i>=j) return dp[i][j]=true;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]==s[j])return solve(s,i+1,j-1);
-
-        return dp[i][j]=false;
-    }
+    
     string longestPalindrome(string s) {
         int n=s.size();
-        int maxlen=0;
-        int startidx=-1;
-        dp.assign(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(s,i,j)&&j-i+1>maxlen){
-                    maxlen=j-i+1;
+        if (n <= 1) return s;
+        int maxlen=1;
+        int startidx=0;
+        vector<vector<int>>dp(n,vector<int>(n,false));
+        for(int l=1;l<=n;l++){
+            for(int i=0;i+l-1<n;i++){
+                int j=i+l-1;
+                if(i==j)dp[i][j]=true;
+                else if(i+1==j && s[i]==s[j])dp[i][j]=true;
+                else if(s[i]==s[j] && dp[i+1][j-1])dp[i][j]=true;
+
+                if(dp[i][j] && l>maxlen){
+                    maxlen=l;
                     startidx=i;
                 }
             }
