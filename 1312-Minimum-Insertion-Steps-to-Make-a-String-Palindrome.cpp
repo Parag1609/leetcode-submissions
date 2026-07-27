cee418fc982e1a6b692dc521a531694d4b94f54a
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-27 10:36:12 +0530

    [1312] [Time Beats: 77.49%] [Memory Beats: 71.12%] - LeetPush

--- 1312-Minimum-Insertion-Steps-to-Make-a-String-Palindrome.cpp
+++ 1312-Minimum-Insertion-Steps-to-Make-a-String-Palindrome.cpp
@@ -1,3 +1,5 @@
+/*
+//recursion+memoization
 class Solution {
 public:
     vector<vector<int>>dp;
@@ -18,5 +20,26 @@ public:
         if(n<=1)return 0;
         dp.assign(n,vector<int>(n,-1));
         return solve(s,0,n-1);
+    }
+};
+*/
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int minInsertions(string s) {
+        int n=s.size();
+        if(n<=1)return 0;
+        dp.assign(n,vector<int>(n,0));
+        
+        for(int l=2;l<=n;l++){
+            for(int i=0;i+l-1<n;i++){
+                int j=i+l-1;
+                if(s[i]==s[j]) dp[i][j]=dp[i+1][j-1];
+                else{
+                    dp[i][j]=1+min(dp[i][j-1],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 };
