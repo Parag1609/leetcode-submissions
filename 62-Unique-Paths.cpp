7564adf9d1b17905a950b3e42310b1aed12c67c2
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-28 20:44:38 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 54.67%] - LeetPush

--- 62-Unique-Paths.cpp
+++ 62-Unique-Paths.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solve(int i,int j,int m,int n){
-        if(i>=m || j>=n)return 0;
-        if(i==m-1 &&j==n-1)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
-
-        int right=solve(i,j+1,m,n);
-        int down=solve(i+1,j,m,n);
-
-        return dp[i][j]= down+right;
-    }
+    
     int uniquePaths(int m, int n) {
-        dp.assign(m,vector<int>(n,-1));
-        return solve(0,0,m,n);
+        dp.assign(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
