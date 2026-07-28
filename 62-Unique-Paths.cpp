a7de35477c2c2e171e2cacbd1209634642572733
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-28 23:10:47 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 82.80%] - LeetPush

--- 62-Unique-Paths.cpp
+++ 62-Unique-Paths.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
+    
     
     int uniquePaths(int m, int n) {
-        dp.assign(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int j=0;j<n;j++){
-            dp[0][j]=1;
-        }
+        vector<int>prev(n,1);
+        
+        
         for(int i=1;i<m;i++){
+            vector<int>curr(n,1);
             for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                curr[j]=prev[j]+curr[j-1];
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
