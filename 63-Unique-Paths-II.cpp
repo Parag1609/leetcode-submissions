ae140469450110565d11809ebf6638f25d8ee903
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-07-29 23:09:46 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 90.91%] - LeetPush

--- 63-Unique-Paths-II.cpp
+++ 63-Unique-Paths-II.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>>dp;
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
@@ -21,5 +21,33 @@ public:
             }
         }
         return dp[row-1][col-1];
+    }
+};
+*/
+class Solution {
+public:
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int row=obstacleGrid.size();
+        int col=obstacleGrid[0].size();
+
+        vector<int>prev(col,0);
+
+        
+        for(int j=0;j<col;j++){
+            if(obstacleGrid[0][j]==1)break;
+            prev[j]=1;
+        }
+        for(int i=1;i<row;i++){
+            vector<int>curr(col,0);
+            if (obstacleGrid[i][0] == 0) {
+                curr[0] = prev[0]; 
+            }
+            for(int j=1;j<col;j++){
+                if(obstacleGrid[i][j]!=1)curr[j]=curr[j-1]+prev[j];
+            }
+            prev=curr;
+        }
+        return prev[col-1];
     }
 };
