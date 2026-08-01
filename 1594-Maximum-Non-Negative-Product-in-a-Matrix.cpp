fea7c226d183e03796bffb525ae84d88df250a1e
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-01 16:31:38 +0530

    [1594] [Time Beats: 10.59%] [Memory Beats: 29.53%] - LeetPush

---
+++ 1594-Maximum-Non-Negative-Product-in-a-Matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int m,n;
+    typedef long long ll;
+    int MOD=1e9+7;
+    vector<vector<pair<ll,ll>>>dp;
+    pair<ll,ll> solve(int i,int j,vector<vector<int>>& grid){
+        int val=grid[i][j];
+        if(i==m-1 && j==n-1)return {val,val};
+        if(dp[i][j]!= pair<ll, ll>{LLONG_MAX,LLONG_MIN} )return dp[i][j];
+        else if(i==m-1){
+            auto right=solve(i,j+1,grid);
+            return dp[i][j]= {min(val*right.first,val*right.second),max(val*right.first,val*right.second)};
+        }
+        else if(j==n-1){
+            auto down=solve(i+1,j,grid);
+            return dp[i][j]={min(val*down.first,val*down.second),max(val*down.first,val*down.second)};
+        }
+
+        auto down=solve(i+1,j,grid);
+        auto right=solve(i,j+1,grid);
+        
+        ll p1=grid[i][j]*down.first;
+        ll p2=grid[i][j]*down.second;
+        ll p3=grid[i][j]*right.first;
+        ll p4=grid[i][j]*right.second;
+       
+        ll minval=min({p1,p2,p3,p4});
+        ll maxval=max({p1,p2,p3,p4});
+    
+        return dp[i][j]={minval,maxval};
+    }
+    int maxProductPath(vector<vector<int>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        dp.assign(m,vector<pair<ll,ll>>(n,{LLONG_MAX,LLONG_MIN}));
+        pair<ll,ll>p= solve(0,0,grid);
+        return p.second<0?-1:p.second % MOD;
+    }
+};
