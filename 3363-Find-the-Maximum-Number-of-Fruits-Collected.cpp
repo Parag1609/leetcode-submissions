684a58a1b0e3f01c89c86c7dc6f9e53b4f0ea852
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-03 01:15:49 +0530

    [3363] [Time Beats: 92.97%] [Memory Beats: 27.08%] - LeetPush

--- 3363-Find-the-Maximum-Number-of-Fruits-Collected.cpp
+++ 3363-Find-the-Maximum-Number-of-Fruits-Collected.cpp
@@ -2,6 +2,52 @@ class Solution {
 public:
     int n;
     vector<vector<int>>dp2;
+    vector<vector<int>>dp3;
+    int solve3(int i,int j,vector<vector<int>>& fruits){
+        for(int j=1;j<n;j++){
+            for(int i=j+1;i<n;i++){
+                int prevmax=dp3[i-1][j-1];
+                prevmax=max(prevmax,dp3[i][j-1]);
+                if(i+1<n)prevmax=max(prevmax,dp3[i+1][j-1]);
+                if(prevmax>-1e9)dp3[i][j]=fruits[i][j]+prevmax;
+            }
+        }
+        return dp3[n-1][n-2];
+        
+    }
+    int solve2(int i,int j,vector<vector<int>>& fruits){
+        for(int i=1;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int prevmax=dp2[i-1][j-1];
+                prevmax=max(prevmax,dp2[i-1][j]);
+                if(j+1<n)prevmax=max(prevmax,dp2[i-1][j+1]);
+
+                if(prevmax>-1e9)dp2[i][j]=prevmax+fruits[i][j];
+            }
+        }
+        return dp2[n-2][n-1];
+    }
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        n=fruits.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=fruits[i][i];
+        }
+        dp2.assign(n,vector<int>(n,-1e9));
+        dp3.assign(n,vector<int>(n,-1e9));
+        dp2[0][n-1]=fruits[0][n-1];
+        dp3[n-1][0]=fruits[n-1][0];
+        
+        return ans+solve2(0,n-1,fruits)+solve3(n-1,0,fruits);
+
+    }
+};
+
+/*
+class Solution {
+public:
+    int n;
+    vector<vector<int>>dp2;
     //vector<vector<int>>dp3;
     int solve2(int i,int j,vector<vector<int>>& fruits){
         if(i>=n ||i<0||j<0|| j>=n)return -1e9;
@@ -41,7 +87,7 @@ public:
 
     }
 };
-/*class Solution {
+class Solution {
 public:
     int n;
     vector<vector<int>>dp2;
