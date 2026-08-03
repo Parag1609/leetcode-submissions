db8dabf8ea8ab260e1b91eda7d70058e9b110f7c
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-03 17:03:42 +0530

    [416] [Time Beats: 27.25%] [Memory Beats: 23.49%] - LeetPush

--- 416-Partition-Equal-Subset-Sum.cpp
+++ 416-Partition-Equal-Subset-Sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int n;
+    vector<vector<int>>dp;
     bool solveSubsetSum(int n, vector<int>& nums ,int target_sum){
         if(target_sum==0)return true;
         if(n<0)return false;
-
+        if(dp[n][target_sum]!=-1)return dp[n][target_sum];
         if(nums[n]>target_sum){
-            return solveSubsetSum(n-1,nums,target_sum);
+            return dp[n][target_sum]=solveSubsetSum(n-1,nums,target_sum);
         }
         bool take=solveSubsetSum(n-1,nums,target_sum-nums[n]);
         bool skip=solveSubsetSum(n-1,nums,target_sum);
-        return take||skip;
+        return dp[n][target_sum]=take||skip;
     }
     bool canPartition(vector<int>& nums) {
         n=nums.size();
@@ -20,7 +21,7 @@ public:
         }
         if(sum%2!=0)return false;
         int target_sum=sum/2;
-
+        dp.assign(n,vector<int>(target_sum+1,-1));
         return solveSubsetSum(n-1,nums,target_sum);
     }
 };
