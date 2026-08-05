2a5b41d3ab02b45328258e12d8c5e25da7532e66
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-05 22:09:03 +0530

    [219] [Time Beats: 14.14%] [Memory Beats: 26.71%] - LeetPush

--- 219-Contains-Duplicate-II.cpp
+++ 219-Contains-Duplicate-II.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
+        unordered_set<int>s;
         for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])!=mp.end()){
-                if(abs(mp[nums[i]]-i)<=k)return true;
-            }
-                mp[nums[i]]=i;
+           if(s.contains(nums[i]))return true;
+           s.insert(nums[i]);
+           if(s.size()>k){
+            s.erase(nums[i-k]);
+           }
             
         }
         return false;
