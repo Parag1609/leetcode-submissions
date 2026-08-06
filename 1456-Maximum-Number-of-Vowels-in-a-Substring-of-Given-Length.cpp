a274d3e2a1f060fa2210a6967d75390ffa4432b9
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-06 12:52:59 +0530

    [1456] [Time Beats: 63.47%] [Memory Beats: 28.50%] - LeetPush

--- 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.cpp
+++ 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    bool isVowel(char ch){
-        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')return true;
-        return false;
-    }
+    
 
     int maxVowels(string s, int k) {
         int maxcount=0;
         int currwindowcount=0;
         int i=0,j=0,n=s.length();
+        auto isVowel=[&](char ch){
+            return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+        };
         while(j<n){
             if(isVowel(s[j]))currwindowcount++;
             if(j-i+1==k){
                 maxcount=max(maxcount,currwindowcount);
+                if (maxcount == k) return k;
                 if(isVowel(s[i]))currwindowcount--;
                 i++;
             }
