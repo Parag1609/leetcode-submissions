2a88e5bf524224c531da1cde91a54908942afb25
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-07 08:29:31 +0530

    [1004] [Time Beats: 33.04%] [Memory Beats: 65.80%] - LeetPush

--- 1004-Max-Consecutive-Ones-III.cpp
+++ 1004-Max-Consecutive-Ones-III.cpp
@@ -3,6 +3,24 @@ public:
     int longestOnes(vector<int>& nums,int k) {
         int n=nums.size();
         int zcount=0;
+        int left=0,right=0;
+        while(right<n){
+            if(nums[right]==0)zcount++;
+            if(zcount>k){
+                if(nums[left]==0)zcount--;
+                left++;
+            }
+            right++;
+        }
+        return right-left;
+    }
+};
+
+/*class Solution {
+public:
+    int longestOnes(vector<int>& nums,int k) {
+        int n=nums.size();
+        int zcount=0;
         int max1s=0;
         int left=0,right=0;
         while(right<n){
@@ -17,3 +35,4 @@ public:
         return max1s;
     }
 };
+*/
