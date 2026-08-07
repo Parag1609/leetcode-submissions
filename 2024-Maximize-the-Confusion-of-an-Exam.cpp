31313ae29b5057c95b53b3c5d479e04b7e1f8ffc
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-07 15:16:27 +0530

    [2024] [Time Beats: 92.06%] [Memory Beats: 78.60%] - LeetPush

--- 2024-Maximize-the-Confusion-of-an-Exam.cpp
+++ 2024-Maximize-the-Confusion-of-an-Exam.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int maxHelper(string answerKey, int k,char ch) {
-        int n=answerKey.length();
-        int flipcount=0;
-        int left=0,right=0;
-        int maxlen=0;
-
-        while(right<n){
-            if(answerKey[right]==ch)flipcount++;
-            
-            while(flipcount>k){
-                if(answerKey[left]==ch)flipcount--;
+    
+    int maxConsecutiveAnswers(string answerKey, int k) {
+       int countT=0,countF=0,left=0,right=0;
+       int maxlen=0;
+       int n=answerKey.size();
+       while(right<n){
+            if(answerKey[right]=='F')countF++;
+            else countT++;
+            while(min(countF,countT)>k){
+                if(answerKey[left]=='F')countF--;
+                else countT--;
                 left++;
             }
             maxlen=max(maxlen,right-left+1);
             right++;
-        }
-        return maxlen;
-    }
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        int flipT= maxHelper(answerKey,k,'T');
-        int flipF= maxHelper(answerKey,k,'F');
-        return max(flipT,flipF);
+       }
+       return maxlen;
     }
 };
