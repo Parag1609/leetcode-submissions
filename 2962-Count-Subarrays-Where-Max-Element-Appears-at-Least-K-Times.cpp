5406a8f7bee0709f6d02edd7bb91d5347e3e9ebd
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-10 01:06:45 +0530

    [2962] [Time Beats: 70.32%] [Memory Beats: 63.16%] - LeetPush

--- 2962-Count-Subarrays-Where-Max-Element-Appears-at-Least-K-Times.cpp
+++ 2962-Count-Subarrays-Where-Max-Element-Appears-at-Least-K-Times.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    long long atMost(vector<int>& nums, int k,int n){
-        long long maxi=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>maxi){
-                maxi=nums[i];
-            }
-        }
 
+    long long countSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
         int i=0,j=0;
-        long long count=0,maxifreq=0;
+        long long count=0;
+        int maxi=0;
+        long long maxifreq=0;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,nums[i]);
+        }
 
         while(j<n){
             if(nums[j]==maxi)maxifreq++;
-            while(maxifreq>k){
+            while(maxifreq==k){
                 if(nums[i]==maxi)maxifreq--;
                 i++;
             }
-            count+=j-i+1;
+            count+=i;
             j++;
+
         }
         return count;
-    }
-    long long countSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        return 1LL*n*(n+1)/2 - atMost(nums,k-1,n);
     }
 };
