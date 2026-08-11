4f71e9e3f6523553610748edf093b0632effd21b
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-12 00:50:40 +0530

    [1052] [Time Beats: 100.00%] [Memory Beats: 98.04%] - LeetPush

--- 1052-Grumpy-Bookstore-Owner.cpp
+++ 1052-Grumpy-Bookstore-Owner.cpp
@@ -3,18 +3,20 @@ public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int n=customers.size();
         int i=0,j=0,satisfied=0,maxcustomers=0;
-
+        for(;j<minutes;j++){
+            satisfied+=grumpy[j]*customers[j];
+        }
+        maxcustomers=satisfied;
         while(j<n){
-            satisfied+=grumpy[j]==1?customers[j]:0;
-            while(j-i+1>minutes){
-                satisfied-=grumpy[i]==1?customers[i]:0;
-                i++;
-            }
+            satisfied+=grumpy[j]*customers[j];
+            satisfied-=grumpy[i]*customers[i];
+
             maxcustomers=max(maxcustomers,satisfied);
             j++;
+            i++;
         }
         for(int x=0;x<n;x++){
-            maxcustomers+=grumpy[x]==1?0:customers[x];
+            maxcustomers+=(1-grumpy[x])*customers[x];
         }
         return maxcustomers;
     }
