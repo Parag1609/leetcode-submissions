8745c161139265791836a54a117aa540484ca0fd
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-08-12 17:50:42 +0530

    [2134] [Time Beats: 22.39%] [Memory Beats: 53.48%] - LeetPush

--- 2134-Minimum-Swaps-to-Group-All-1's-Together-II.cpp
+++ 2134-Minimum-Swaps-to-Group-All-1's-Together-II.cpp
@@ -6,16 +6,12 @@ public:
         for(int num:nums){
             totalones+=num==1?1:0;
         }
-        vector<int>temp(2*n);
-        for(int i=0;i<2*n;i++){
-            temp[i]=nums[i%n];
-        }
-
+        
         int i=0,minswaps=INT_MAX,currones=0;
         for(int j=0;j<2*n;j++){
-            if(temp[j]==1)currones++;
-            while(j-i+1>totalones){
-                if(temp[i]==1)currones--;
+            if(nums[j%n]==1)currones++;
+            if(j-i+1>totalones){
+                if(nums[i%n]==1)currones--;
                 i++;
             }
             minswaps=min(minswaps,totalones-currones);
