ebee90e867d5ac9b5e5b798666adcbe18c5be28b
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-09-02 17:23:33 +0530

    [1498] [Time Beats: 84.07%] [Memory Beats: 68.13%] - LeetPush

---
+++ 1498-Number-of-Subsequences-That-Satisfy-the-Given-Sum-Condition.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int M=1e9+7;
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int l=0,r=n-1;
+        int result=0;
+        vector<int>power(n,1);
+        for(int i=1;i<n;i++){
+            power[i]=(power[i-1]*2)%M;
+        }
+        while(l<=r){
+            if(nums[l]+nums[r]<=target){
+                result=(result%M +power[r-l]%M)%M;
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return result;
+    }
+};
